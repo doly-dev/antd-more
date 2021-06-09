1cec4c8ece29fbf38bc6d16a0d8f70ec35e98c39
蔡金锋 <jeff.cai@99bill.com>
2021-06-10 00:25:49 +0800

    refactor: 重新梳理，并自动生成文档

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -7,23 +7,13 @@ export default {
     type: 'babel',
     lazy: true
   },
-  // 如要开启，需将 https://github.com/umijs/fabric/blob/master/tsconfig.json 存到根目录
-  // umi: {
-  //   name: 'antdMore',
-  //   file: 'antd-more',
-  //   globals: {
-  //     react: 'React',
-  //     antd: 'antd'
-  //   },
-  //   sourcemap: true,
-  //   minFile: true
-  // },
+  runtimeHelpers: true,
   extraBabelPlugins: [
     [
       'import',
       {
         libraryName: 'antd',
-        libraryDirectory: 'es',
+        libraryDirectory: 'lib',
         style: true
       }
     ]
