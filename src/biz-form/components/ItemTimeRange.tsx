da63c9dfb08fad9977e3f09c1ed1d1ed56193566
caijf <369756941@qq.com>
2023-06-25 14:56:00 +0800

    refactor: 内部ID前缀调整

--- src/biz-form/components/ItemTimeRange.tsx
+++ src/biz-form/components/ItemTimeRange.tsx
@@ -48,7 +48,7 @@ const BizFormItemTimeRange: React.FC<BizFormItemTimeRangeProps> = ({
 }) => {
   const hasNames = React.useMemo(() => Array.isArray(names) && names.length > 0, [names]);
   const currentName = React.useMemo(
-    () => name || (hasNames ? uniqueId('cascader') : name),
+    () => name || (hasNames ? uniqueId('__am_timeRange_') : name),
     [hasNames, name]
   );
   const currentFormat = React.useMemo(() => {
--- src/biz-table/_util/actionCache.ts
+++ src/biz-table/_util/actionCache.ts
@@ -2,7 +2,7 @@ import { uniqueId } from 'ut2';
 
 // 创建一个操作缓存key
 function createActionCacheKey() {
-  return uniqueId('bizTable');
+  return uniqueId('__am_bizTable_');
 }
 
 const actionCache = {};
